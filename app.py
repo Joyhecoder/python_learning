ddb229e79ac728255c9137ac1a03c35a8af8ea4c
Joy He <joyhe520@gmail.com>
2023-07-17 19:42:34 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+
+#? declare a variable 
+age = 20
+price = 19.95
+first_name = "Joy"
+is_online = True
+# print(age)
+
+#? input
+# name = input("what is your name? >>")
+# print("Hello, " + name)
+
+# birth_year = input("Enter your birth year: ")
+# birth_year_int = int(birth_year)
+# age = 2023 - birth_year_int
+# print(age)
+
+#? converting 
+int()
+float()
+bool()
+str()
+
+#? first_num = input("enter a number: ")
+# second_num = input("enter a second number: ")
+# sum = float(first_num) + float(second_num)
+# print(f"{first_num} plus {second_num} is {sum}")
+
+#? object methods
+course = 'Python for Beginners'
+print(course.upper())
+print(course.lower())
+print(course.find('for')) #7
+print(course.find('Y')) #-1
+print(course.replace('for', '4'))
+print(course)
